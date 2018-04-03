6ce513465936eec75de715acc63d82ce914f975a
wzetao <wzetao@126.com>
2018-04-03 23:39:18 +0800

    优化getWikiLinks.py

--- WebScrapingWithPython_RyanMitchell/lesson3/1-getWikiLinks.py
+++ WebScrapingWithPython_RyanMitchell/lesson3/1-getWikiLinks.py
@@ -1,8 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
 html = urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
 bsObj = BeautifulSoup(html)
-for link in bsObj.findAll("a"):
+for link in bsObj.find("div", {"id":"bodyContent"}).findAll("a", href = re.compile("^(/wiki/)((?!:).)*$")):
     if 'href' in link.attrs:
         print(link.attrs['href'])
