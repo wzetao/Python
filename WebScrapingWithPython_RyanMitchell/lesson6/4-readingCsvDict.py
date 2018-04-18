756d9b1df29eb75def2e5cf69140a2fc47d9bdc5
wzetao <wzetao@126.com>
2018-04-19 02:27:50 +0800

    修改4-readingCsvDict.py文件

--- WebScrapingWithPython_RyanMitchell/lesson6/4-readingCsvDict.py
+++ WebScrapingWithPython_RyanMitchell/lesson6/4-readingCsvDict.py
@@ -1,22 +1,12 @@
 from urllib.request import urlopen
-from pdfminer.pdfinterp import PDFResourceManager, process_pdf
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
 from io import StringIO
-from io import open
+import csv
 
-def readPDF(pdfFile):
-    rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
-    process_pdf(rsrcmgr, device, pdfFile)
-    device.close()
-    content = retstr.getvalue()
-    retstr.close()
-    return content
+data = urlopen("http://pythonscraping.com/files/MontyPythonAlbums.csv").read().decode('ascii', 'ignore')
+dataFile = StringIO(data)
+dictReader = csv.DictReader(dataFile)
 
-pdfFile = urlopen("http://pythonscraping.com/pages/warandpeace/chapter1.pdf")
-outputString = readPDF(pdfFile)
-print(outputString)
-pdfFile.close()
+print(dictReader.fieldnames)
+
+for row in dictReader:
+    print(row)
