bbda377db3094f37820fc7eb7091fcdc07bdf9c0
wzetao <wzetao@126.com>
2018-04-29 16:49:57 +0800

    修改1-wikiUnitTest.py文件

--- WebScrapingWithPython_RyanMitchell/lesson13/1-wikiUnitTest.py
+++ WebScrapingWithPython_RyanMitchell/lesson13/1-wikiUnitTest.py
@@ -1,24 +1,42 @@
-from urllib.request import urlopen
+from urllib.request import urlopen,unquote
 from bs4 import BeautifulSoup
 import unittest
 
 class TestWikipedia(unittest.TestCase):
     bsObj = None
+    url = None
 
-    @classmethod
-    def setUpClass(cls):
+    def test_PageProperties(self):
         global bsObj
-        url = "http://en.wikipedia.org/wiki/Monty_Python"
-        bsObj = BeautifulSoup(urlopen(url))
+        global url
 
-    def test_titleText(self):
+        url =  "http://en.wikipedia.org/wiki/Monty_Python"
+        for i in range(1, 100):
+            bsObj = BeautifulSoup(urlopen(url))
+            titles = self.titleMatchesURL()
+            self.assertEqual(titles[0], titles[1])
+            self.assertTrue(self.contentExists())
+            url = self.getNextLink()
+        print("Done!")
+
+
+    def titleMatchesURL(self):
         global bsObj
+        global url
         pageTitle = bsObj.find("h1").get_text()
-        self.assertEqual("Monty Python", pageTitle)
+        urlTitle = url[(url.index("/wiki/") + 6):]
+        urlTitle = urlTitle.replace("_", " ")
+        urlTitle = unquote(urlTitle)
+        return [pageTitle.lower(), urlTitle.lower()]
 
-    def test_contentExists(self):
+    def contentExists(self):
         global bsObj
-        content = bsObj.find("div",{"id":"mw-content-text"})
-        self.assertIsNotNone(content)
+        content = bsObj.find("div", {"id":"mv-content-text"})
+        if content is not None:
+            return True
+        return False
+
+    def getNextLink(self):
+        pass
 if __name__ == '__main__':
     unittest.main()
